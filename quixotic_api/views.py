2a09e268d3477a8dc60ca11ba701f1704a138e6f
Zach Carwile <zachary.carwile@gmail.com>
2017-08-30 17:32:06 -0400

    fixed comments

--- quixotic_api/views.py
+++ quixotic_api/views.py
@@ -4,21 +4,21 @@ from quixotic_api.serializers import EventSerializer, TimeblockSerializer, Proje
 
 
 class EventViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Chain objects """
+    """ ViewSet for viewing and editing Event objects """
     queryset = Event.objects.all()
     serializer_class = EventSerializer
 
 class TimeblockViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Store objects """
+    """ ViewSet for viewing and editing Timeblock objects """
     queryset = Timeblock.objects.all()
     serializer_class = TimeblockSerializer
 
 class ProjectViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Chain objects """
+    """ ViewSet for viewing and editing Project objects """
     queryset = Project.objects.all()
     serializer_class = ProjectSerializer
 
 class UserViewSet(viewsets.ModelViewSet):
-    """ ViewSet for viewing and editing Store objects """
+    """ ViewSet for viewing and editing User objects """
     queryset = User.objects.all()
     serializer_class = UserSerializer
